04e7e051383d415baf75fd692a0062e8351b1de3
Barbara Swift <barbswift@verizon.net>
2012-08-21 16:53:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '159789a848373d8454d2470655649716cbb884577225f6550f0db324e8c665b6f200493dc60ad5b800992c47359aa4e40cf0ec8187e510668dc4accedc3e95c6'
